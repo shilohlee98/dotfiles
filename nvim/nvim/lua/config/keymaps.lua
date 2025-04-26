00f5ed494dc4d57ecebb3c73b8a3bb68a5855d93
Shiloh Lee <as97108@gmail.com>
2025-04-26 22:36:47 +0800

    Update nvim keymap

--- nvim/nvim/lua/config/keymaps.lua
+++ nvim/nvim/lua/config/keymaps.lua
@@ -6,3 +6,6 @@ vim.keymap.set("n", "yy", '"+yy')
 
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
+
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
