da0d6e41f59a9b85fc667ea4b6b53ed28ba9d8cf
Shiloh Lee <as97108@gmail.com>
2025-04-28 11:29:32 +0800

    Update keymap config

--- nvim/nvim/lua/config/keymaps.lua
+++ nvim/nvim/lua/config/keymaps.lua
@@ -1,8 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set({ "n", "v" }, "y", '"+y')
-vim.keymap.set("n", "yy", '"+yy')
+vim.keymap.set({ "n", "v" }, "<leader>y", '"+y')
+vim.keymap.set("n", "<leader>yy", '"+yy')
 
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
---
+++ nvim/nvim/lua/plugins/surr.lua
@@ -0,0 +1,23 @@
+return {
+    "kylechui/nvim-surround",
+    version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+            keymaps = {
+                insert = "<C-g>s",
+                insert_line = "<C-g>S",
+                normal = "s",
+                normal_cur = "ss",
+                normal_line = "S",
+                normal_cur_line = "SS",
+                visual = "S",
+                visual_line = "gS",
+                delete = "ds",
+                change = "cs",
+                change_line = "cS",
+            },
+        })
+    end,
+}
