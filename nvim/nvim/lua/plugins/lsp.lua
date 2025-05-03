632c52f9d5195a179d734ba5e5da448c6b6a8d9e
Shiloh Lee <as97108@gmail.com>
2025-05-03 17:44:47 +0800

    Change picker from telescope to snack.picker

--- nvim/nvim/lua/plugins/lsp.lua
+++ nvim/nvim/lua/plugins/lsp.lua
@@ -1,38 +1,18 @@
-vim.keymap.set("n", "<Leader>d", vim.diagnostic.open_float)
-vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
-vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
-vim.diagnostic.config({
-    float = { border = "rounded" },
-})
-
-vim.diagnostic.config({
-    virtual_text = {
-        prefix = "●",
-        spacing = 4,
-        severity = nil,
-    },
-})
-
-vim.api.nvim_create_autocmd("LspAttach", {
-    group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-    callback = function(ev)
-        vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
-
-        local opts = { buffer = ev.buf }
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-        vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
-        vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-        vim.keymap.set("n", "<Leader>a", vim.lsp.buf.code_action, { noremap = true, silent = true })
-    end,
-})
-
 return {
     {
         "neovim/nvim-lspconfig",
         event = "VeryLazy",
+        config = function()
+            vim.diagnostic.config({
+                float = { border = "rounded" },
+                virtual_text = {
+                    prefix = "●",
+                    spacing = 4,
+                },
+            })
+        end,
     },
+
     {
         "williamboman/mason.nvim",
         event = "VeryLazy",
@@ -40,32 +20,49 @@ return {
             require("mason").setup()
         end,
     },
+
     {
         "williamboman/mason-lspconfig.nvim",
         event = "VeryLazy",
-        dependencies = { "mason.nvim" },
+        dependencies = {
+            "mason.nvim",
+            "neovim/nvim-lspconfig",
+        },
         config = function()
-            require("mason-lspconfig").setup()
-            require("mason-lspconfig").setup_handlers({
-                function(server_name)
-                    require("lspconfig")[server_name].setup({})
-                end,
-            })
-        end,
+            local capabilities = vim.lsp.protocol.make_client_capabilities()
+            capabilities.offsetEncoding = { "utf-16" }
+            local lspconfig = require("lspconfig")
 
-        ["pyright"] = function()
-            lspconfig.pyright.setup({
-                on_attach = function(client)
-                    client.server_capabilities.documentFormattingProvider = false
-                    client.server_capabilities.documentRangeFormattingProvider = false
+            require("mason-lspconfig").setup({
+                ensure_installed = { "pyright", "lua_ls" },
+                automatic_installation = true,
+            })
+
+            require("mason-lspconfig").setup_handlers({
+                -- Default handler
+                function(server_name)
+                    lspconfig[server_name].setup({
+                        capabilities = capabilities,
+                    })
                 end,
-                settings = {
-                    python = {
-                        analysis = {
-                            typeCheckingMode = "off",
+
+                -- Custom pyright handler
+                ["pyright"] = function()
+                    lspconfig.pyright.setup({
+                        capabilities = capabilities,
+                        settings = {
+                            python = {
+                                analysis = {
+                                    typeCheckingMode = "off",
+                                },
+                            },
                         },
-                    },
-                },
+                        on_attach = function(client)
+                            client.server_capabilities.documentFormattingProvider = false
+                            client.server_capabilities.documentRangeFormattingProvider = false
+                        end,
+                    })
+                end,
             })
         end,
     },
