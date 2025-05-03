a968a95352e10009720d880c4ce81e0376883f73
Shiloh Lee <as97108@gmail.com>
2025-05-03 23:56:25 +0800

    Update md preview keymap

--- nvim/nvim/lua/plugins/md.lua
+++ nvim/nvim/lua/plugins/md.lua
@@ -3,6 +3,9 @@ return {
     event = "VeryLazy",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     build = "cd app && yarn install",
+    keys = {
+        { "<leader>mp", "<cmd>MarkdownPreviewToggle<cr>", desc = "Markdown Preview" },
+    },
     init = function()
         vim.g.mkdp_filetypes = { "markdown" }
     end,
