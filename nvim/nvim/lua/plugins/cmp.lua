c8ecb68e3d853350128d013bacbd8939183be415
shilohlee <shilohlee98@gmail.com>
2025-06-01 14:55:38 +0800

    Update cmp tab config

--- nvim/nvim/lua/plugins/cmp.lua
+++ nvim/nvim/lua/plugins/cmp.lua
@@ -27,7 +27,22 @@ return {
             -- C-k: Toggle signature help (if signature.enabled = true)
             --
             -- See :h blink-cmp-config-keymap for defining your own keymap
-            keymap = { preset = "default" },
+            keymap = {
+                preset = "default",
+                ['<Tab>'] = {
+                    function(cmp)
+                        if cmp.snippet_active() then
+                            return cmp.accept()
+                        else
+                            return cmp.select_and_accept()
+                        end
+                    end,
+                    'snippet_forward',
+                    'fallback'
+                },
+                ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
+            },
+
 
             appearance = {
                 -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
