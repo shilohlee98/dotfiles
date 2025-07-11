0de0284a6ebc11bd82335441870a388545ae2799
shilohlee <shilohlee98@gmail.com>
2025-07-12 00:17:01 +0800

    Update nvim line config

--- nvim/nvim/lua/config/options.lua
+++ nvim/nvim/lua/config/options.lua
@@ -4,6 +4,7 @@ vim.opt.expandtab = true
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.showmode = false
+vim.opt.cmdheight = 0
 vim.api.nvim_create_autocmd("FileType", {
     pattern = { "javascript", "typescript", "javascriptreact", "typescriptreact" },
     callback = function()
--- nvim/nvim/lua/plugins/lines.lua
+++ nvim/nvim/lua/plugins/lines.lua
@@ -53,33 +53,6 @@ return {
     },
 
     {
-        "folke/noice.nvim",
-        event = "VeryLazy",
-        opts = {
-            views = {
-                cmdline_popup = {
-                    position = {
-                        row = 3,
-                        col = "50%",
-                    },
-                    size = {
-                        width = 60,
-                        height = "auto",
-                    },
-                },
-            },
-            cmdline = {
-                enabled = true,
-                view = "cmdline_popup",
-            },
-        },
-        dependencies = {
-            "MunifTanjim/nui.nvim",
-            "rcarriga/nvim-notify",
-        },
-    },
-
-    {
         "akinsho/bufferline.nvim",
         event = "VeryLazy",
         dependencies = { "nvim-tree/nvim-web-devicons" },
@@ -94,20 +67,20 @@ return {
             })
         end,
         keys = {
-            { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle Pin" },
+            { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>",  desc = "Toggle Pin" },
             {
                 "<leader>bP",
                 "<Cmd>BufferLineGroupClose ungrouped<CR>",
                 desc = "Delete Non-Pinned Buffers",
             },
             { "<leader>br", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete Buffers to the Right" },
-            { "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete Buffers to the Left" },
-            { "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
-            { "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
-            { "[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
-            { "]b", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
-            { "[B", "<cmd>BufferLineMovePrev<cr>", desc = "Move buffer prev" },
-            { "]B", "<cmd>BufferLineMoveNext<cr>", desc = "Move buffer next" },
+            { "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>",  desc = "Delete Buffers to the Left" },
+            { "<S-h>",      "<cmd>BufferLineCyclePrev<cr>",  desc = "Prev Buffer" },
+            { "<S-l>",      "<cmd>BufferLineCycleNext<cr>",  desc = "Next Buffer" },
+            { "[b",         "<cmd>BufferLineCyclePrev<cr>",  desc = "Prev Buffer" },
+            { "]b",         "<cmd>BufferLineCycleNext<cr>",  desc = "Next Buffer" },
+            { "[B",         "<cmd>BufferLineMovePrev<cr>",   desc = "Move buffer prev" },
+            { "]B",         "<cmd>BufferLineMoveNext<cr>",   desc = "Move buffer next" },
         },
     },
 }
