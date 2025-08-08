1228c5003b75c6f7643d7a0b1ff7c32f7b4af54f
shilohlee98 <shilohlee98@gmail.com>
2025-08-08 23:11:26 +0800

    Update groovy and jenkinsfile format

--- nvim/nvim/lua/config/options.lua
+++ nvim/nvim/lua/config/options.lua
@@ -14,3 +14,12 @@ vim.api.nvim_create_autocmd("FileType", {
         vim.bo.expandtab = true
     end,
 })
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "groovy", "Jenkinsfile" },
+    callback = function()
+        vim.opt_local.shiftwidth = 2
+        vim.opt_local.tabstop = 2
+        vim.opt_local.softtabstop = 2
+        vim.opt_local.expandtab = true
+    end,
+})
